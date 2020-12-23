caac27a9b8d1066198e6471a34f9d2c72db67974
Paul Chang <paulchang959@gmail.com>
2020-12-22 18:31:23 -0800

    final changes to event name display

--- js/script.js
+++ js/script.js
@@ -2,12 +2,11 @@ const eventContainerEl = document.getElementById("events-container");
 const restContainerEl = document.getElementById("restaurant-container");
 const food_KEY = "4e2bcc2c6d960eec2150089303018710";
 const event_KEY = "RpHsNFdNJ9Ukvz7Qw5PwGoIRGwUTzyDP";
-let cityListName = document.getElementById("city-name");
-let restListName = document.getElementById("rest-name");
-const cityTitleEl = document.querySelector(".city-title");
+const cityTitleEl = document.querySelector("#city-name");
+const city = document.getElementById("events").value;
 
-const getData = function (city) {
-  getEvents(city).then((data) => getRestaurants(data.latitude, data.longitude));
+const getData = function () {
+  getEvents().then((data) => getRestaurants(data.latitude, data.longitude));
 };
 
 const getRestaurants = function (lat, lon) {
@@ -22,35 +21,36 @@ const getRestaurants = function (lat, lon) {
       "user-key": food_KEY,
     },
   }).then(function (response) {
-    response.json().then(function (data, city) {
+    response.json().then(function (data) {
       console.log(data);
-      displayRestaurants(data, city);
+      displayRestaurants(data);
       console.log(data.restaurants[0].restaurant.name);
       return;
     });
   });
 };
 
-const getEvents = function (city) {
+const getEvents = function () {
   return new Promise(function (resolve, reject) {
+    const city = document.getElementById("events").value;
     const event_URL =
       "https://app.ticketmaster.com/discovery/v2/events.json?&city=" +
       city +
       "&apikey=RpHsNFdNJ9Ukvz7Qw5PwGoIRGwUTzyDP";
     fetch(event_URL).then(function (response) {
-      response.json().then(function (data, city) {
-        console.log(data, city);
-        displayEvents(data, city);
+      response.json().then(function (data) {
+        console.log(data);
+        displayEvents(data);
         resolve(data._embedded.events[0]._embedded.venues[0].location);
         console.log(city);
       });
     });
-    displayNames();
   });
 };
 
-const displayEvents = function (data, city) {
+const displayEvents = function (data) {
   const events = data._embedded.events;
+  console.log(events);
   for (let i = 0; i < 5; i++) {
     const eventName = events[i].name;
     const eventDate = events[i].dates.start.localDate;

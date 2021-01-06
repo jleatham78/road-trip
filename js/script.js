c5e1dd22a7eb634f0f2ffb4273e6dfbee812fd87
Paul Chang <paulchang959@gmail.com>
2021-01-05 18:24:48 -0800

    finished date sort

--- js/script.js
+++ js/script.js
@@ -5,14 +5,16 @@ const eventCardImageContainerEl = document.getElementById("event-image");
 const eventCardLinkContainerEl = document.getElementById("event-url");
 const restCardImageContainerEl = document.getElementById("rest-image");
 const restCardContainerEl = document.getElementById("rest-details");
-const restCardLinkContainerEl = document.getElementById("rest-url")
+const restCardLinkContainerEl = document.getElementById("rest-url");
 const food_KEY = "4e2bcc2c6d960eec2150089303018710";
 const event_KEY = "RpHsNFdNJ9Ukvz7Qw5PwGoIRGwUTzyDP";
 const cityTitleEl = document.querySelector("#city-name");
 const city = document.getElementById("events").value;
 
-const getData = function () {
-  getEvents().then((data) => getRestaurants(data.latitude, data.longitude));
+const getData = function (city, dateSearch) {
+  getEvents(city, dateSearch).then((data) =>
+    getRestaurants(data.latitude, data.longitude)
+  );
 };
 
 const getRestaurants = function (lat, lon) {
@@ -29,6 +31,7 @@ const getRestaurants = function (lat, lon) {
   }).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
+
       displayRestaurants(data);
       //displayRestDetails(data);
       console.log(data.restaurants[0].restaurant.name);
@@ -37,9 +40,8 @@ const getRestaurants = function (lat, lon) {
   });
 };
 
-const getEvents = function () {
+const getEvents = function (city, dateSearch) {
   return new Promise(function (resolve, reject) {
-    const city = document.getElementById("events").value;
     const event_URL =
       "https://app.ticketmaster.com/discovery/v2/events.json?&city=" +
       city +
@@ -47,7 +49,10 @@ const getEvents = function () {
     fetch(event_URL).then(function (response) {
       response.json().then(function (data) {
         console.log(data);
-        displayEvents(data);
+        const filterEvents = data._embedded.events.filter(function (event) {
+          return event.dates.start.localDate.slice(0, 7) === dateSearch;
+        });
+        displayEvents(filterEvents);
         //displayEventDetails(data);
         resolve(data._embedded.events[0]._embedded.venues[0].location);
         console.log(city);
@@ -56,20 +61,24 @@ const getEvents = function () {
   });
 };
 
-const displayEvents = function (data) {
-  const events = data._embedded.events;
+const displayEvents = function (events) {
+  eventContainerEl.innerHTML = "";
   console.log(events);
-  for (let i = 0; i < 5; i++) {
-    const eventName = events[i].name;
-    const eventDate = events[i].dates.start.localDate;
-    const event = events[i];
-    const eventDiv = document.createElement("a");
-    eventDiv.setAttribute("class", "collection-item")
-    eventDiv.innerHTML += `Name: ${eventName}<br />Date: ${eventDate}`;
-    eventDiv.onclick = function () {
-      displayEventDetails(event);
-    };
-    eventContainerEl.appendChild(eventDiv);
+  if (events.length) {
+    for (let i = 0; i < events.length; i++) {
+      const eventName = events[i].name;
+      const eventDate = events[i].dates.start.localDate;
+      const event = events[i];
+      const eventDiv = document.createElement("a");
+      eventDiv.setAttribute("class", "collection-item");
+      eventDiv.innerHTML += `Name: ${eventName}<br />Date: ${eventDate}`;
+      eventDiv.onclick = function () {
+        displayEventDetails(event);
+      };
+      eventContainerEl.appendChild(eventDiv);
+    }
+  } else {
+    eventContainerEl.textContent = "There are no events for this month!";
   }
 };
 
@@ -78,71 +87,78 @@ const displayNames = function (city) {
 };
 
 const displayRestaurants = function (data) {
+  restContainerEl.innerHTML = "";
   const restaurants = data.restaurants;
   for (let i = 0; i < 5; i++) {
     const restName = restaurants[i].restaurant.name;
     const restCuis = restaurants[i].restaurant.cuisines;
-    const restaurant = restaurants[i].restaurant
+    const restaurant = restaurants[i].restaurant;
     const restDiv = document.createElement("a");
-    restDiv.setAttribute("class", "collection-item")
+    restDiv.setAttribute("class", "collection-item");
     restDiv.innerHTML += `Name: ${restName}<br />Cuisine: ${restCuis}`;
     restDiv.onclick = function () {
       displayRestDetails(restaurant);
     };
     restContainerEl.appendChild(restDiv);
-   }
+  }
 };
 
 const displayEventDetails = function (event) {
+  // save event to localstorage
+  const name = event.name;
+  const genre = event.classifications[0].genre.name;
+  const dates = event.dates.start.localDate;
+  const venue = event._embedded.venues[0].name;
+  const address = event._embedded.venues[0].address.line1;
+  const status = event.dates.status.code;
+  const url = event._embedded.attractions[0].url;
+  const image = event._embedded.venues[0].images[0].url;
 
-      const name = event.name;
-      const genre = event.classifications[0].genre.name;
-      const dates = event.dates.start.localDate;
-      const venue = event._embedded.venues[0].name;
-      const address = event._embedded.venues[0].address.line1;
-      const status = event.dates.status.code;
-      const url = event._embedded.attractions[0].url;
-      const image = event._embedded.venues[0].images[0].url;
+  eventCardContainerEl.innerHTML = "";
+  eventCardLinkContainerEl.innerHTML = "";
+  eventCardImageContainerEl.innerHTML = "";
 
-      eventCardContainerEl.innerHTML = "";
-      eventCardLinkContainerEl.innerHTML = "";
-      eventCardImageContainerEl.innerHTML = "";
-        
-      eventCardContainerEl.appendChild(eventCardImageContainerEl);
-      eventCardImageContainerEl.innerHTML +=`<img class="card-image" src="${image}">`;
-      eventCardContainerEl.innerHTML += `<p class="card-content">Name: ${name}<br />Genre: ${genre}<br/ >Dates: ${dates}<br />Venue: ${venue}<br />Address: ${address}<br />Status: ${status}</p>`;
-      eventCardLinkContainerEl.innerHTML += `<a class="card-action" target= "blank" href=${url}>Purchase Tickets</a>`; 
-      eventCardContainerEl.appendChild(eventCardLinkContainerEl);
-      
- 
-}
+  eventCardContainerEl.appendChild(eventCardImageContainerEl);
+  eventCardImageContainerEl.innerHTML += `<img class="card-image" src="${image}">`;
+  eventCardContainerEl.innerHTML += `<p class="card-content">Name: ${name}<br />Genre: ${genre}<br/ >Dates: ${dates}<br />Venue: ${venue}<br />Address: ${address}<br />Status: ${status}</p>`;
+  eventCardLinkContainerEl.innerHTML += `<a class="card-action" target= "blank" href=${url}>Purchase Tickets</a>`;
+  eventCardContainerEl.appendChild(eventCardLinkContainerEl);
+};
 
 const displayRestDetails = function (restaurant) {
+  // save restaurant to localstorage
+  const restaurantName = restaurant.name;
+  const restAddress = restaurant.location.address;
+  const restPhone = restaurant.phone_numbers;
+  const restTimings = restaurant.timings;
+  const restPrice = restaurant.price_range;
+  const restImage = restaurant.featured_image;
+  const restLink = restaurant.menu_url;
 
-      const restaurantName = restaurant.name;
-      const restAddress = restaurant.location.address;
-      const restPhone = restaurant.phone_numbers;
-      const restTimings = restaurant.timings;
-      const restPrice = restaurant.price_range;
-      const restImage = restaurant.featured_image;
-      const restLink = restaurant.menu_url
+  restCardContainerEl.innerHTML = "";
+  restCardLinkContainerEl.innerHTML = "";
+  restCardImageContainerEl.innerHTML = "";
 
-      restCardContainerEl.innerHTML = "";
-      restCardLinkContainerEl.innerHTML = "";
-      restCardImageContainerEl.innerHTML = "";
-
-      restCardContainerEl.appendChild(restCardImageContainerEl);
-      restCardImageContainerEl.innerHTML +=`<img class="card-image" src="${restImage}">`;
-      restCardContainerEl.innerHTML += `<p class="card-content">Name: ${restaurantName}<br />Address: ${restAddress}<br/ >Phone: ${restPhone}<br />Offerings: ${restTimings}<br />Price: ${restPrice}</p>`;
-      restCardLinkContainerEl.innerHTML += `<a class="card-action" target="blank" href=${restLink}>Menu</a>`;
-      restCardContainerEl.appendChild(restCardLinkContainerEl);
-
-}
+  restCardContainerEl.appendChild(restCardImageContainerEl);
+  restCardImageContainerEl.innerHTML += `<img class="card-image" src="${restImage}">`;
+  restCardContainerEl.innerHTML += `<p class="card-content">Name: ${restaurantName}<br />Address: ${restAddress}<br/ >Phone: ${restPhone}<br />Offerings: ${restTimings}<br />Price: ${restPrice}</p>`;
+  restCardLinkContainerEl.innerHTML += `<a class="card-action" target="blank" href=${restLink}>Menu</a>`;
+  restCardContainerEl.appendChild(restCardLinkContainerEl);
+};
 
 document.getElementById("search").addEventListener("click", function (event) {
   event.preventDefault();
   const city = document.getElementById("events").value;
-  getData(city);
+  // if (!city) {
+
+  // };
+  const dateSearch = document.getElementById("month-search").value;
+  // save city and date to localstorage
+
+  getData(city, dateSearch);
   displayNames(city);
 });
-  
+
+// get a localstorage to be a save button on the card so when the user likes a event or restaurant, they can press the save button to save for next time.
+
+// sorting the events and restaurants by date input to sort the results of both when pressing search events.
